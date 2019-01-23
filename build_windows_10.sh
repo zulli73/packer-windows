2a7fbe99ed9e5041bb812a4f98a5be50bee52d8d
Raphael Zulliger <raphael.zulliger@besi.com>
2019-01-23 20:20:07 +0100

    Slightly modify build script

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/Downloads/17763.1.180914-1434.rs5_release_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso windows_10.json
+packer build --only=virtualbox-iso  windows_10.json
+# --var iso_url=~/Downloads/17763.1.180914-1434.rs5_release_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso
 
